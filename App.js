3a6af2b50b5ff9fcea40382388d97a5acbc5d658
Johan Abde <abdejohan@gmail.com>
2021-02-15 16:20:22 +0100

    Add styling folder structure;   - Add font related styles in typography   - Add color related styles in colors   - Add file for button related styles later on   - Add sizing file for size related styles   - Use Index.js as middleman to import and export style components   - Display all color and font styles in report component for easy use

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+
+// Screens
+import Home from "./screens/Home";
+import IntroPage from "./screens/IntroPage";
+import Scanner from "./screens/Scanner";
+import Report from "./screens/Report";
+import Livsmedelsverket from "./screens/Livsmedelsverket";
+
+const Tab = createMaterialBottomTabNavigator();
+
+const App = () => {
+  const [loaded, setLoaded] = useState(null);
+  setTimeout(function () {
+    setLoaded("loaded");
+  }, 10);
+  return (
+    <>
+      {!loaded ? (
+        <IntroPage />
+      ) : (
+        <>
+          <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen name="Home" component={Home} />
+              <Tab.Screen name="Scan" component={Scanner} />
+              <Tab.Screen name="Report" component={Report} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </>
+      )}
+    </>
+  );
+};
+
+export default App;
