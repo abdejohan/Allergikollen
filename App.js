ded94af547d1708d72829171dbf485c61f0872fb
Johan Abde <abdejohan@gmail.com>
2021-02-19 00:49:41 +0100

    Import Kitten UI library

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { createMaterialBottomTabNavigator } from "@react-navigation/material-bot
 import { Ionicons } from "@expo/vector-icons";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import { MaterialIcons } from "@expo/vector-icons";
-import { API_URL, API_TOKEN } from "@env";
-console.log(API_TOKEN);
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
+import * as eva from "@eva-design/eva";
 
 // these handle screens for both bottom and top navigation
 import Settings from "./screens/Settings";
@@ -32,77 +32,71 @@ const App = () => {
   };
 
   return (
-    <>
+    <ApplicationProvider {...eva} theme={eva.light}>
       {!loaded ? (
         <IntroPage />
       ) : (
-        <>
-          <NavigationContainer>
-            <Tab.Navigator
-              shifting={true}
-              initialRouteName="Home"
-              activeColor="black"
-              inactiveColor="grey"
-              barStyle={{ backgroundColor: "white" }}
-            >
-              <Tab.Screen
-                name="Home"
-                component={HomeScreenNavigator}
-                options={{
-                  tabBarLabel: "Hem",
-                  tabBarIcon: () => (
-                    <AntDesign name="home" size={24} color="black" />
-                  ),
-                }}
-              />
-              <Tab.Screen
-                name="Scan"
-                component={ScannerScreenNavigator}
-                options={{
-                  tabBarBadge: true,
-                  tabBarLabel: "ScannerPage",
-                  tabBarIcon: () => (
-                    <Ionicons name="barcode-outline" size={24} color="black" />
-                  ),
-                }}
-              />
-              <Tab.Screen
-                name="Report"
-                component={ReportScreenNavigator}
-                options={{
-                  tabBarLabel: "Support",
-                  tabBarIcon: () => (
-                    <MaterialIcons
-                      name="support-agent"
-                      size={24}
-                      color="black"
-                    />
-                  ),
-                }}
-              />
-              <Tab.Screen
-                name="Settings"
-                children={() => (
-                  <Settings
-                    selectedAllergens={selectedAllergens}
-                    setSelectedAllergens={setSelectedAllergens}
-                    deleteAllergen={deleteAllergen}
-                  />
-                )}
-                options={{
-                  selectedAllergens: { selectedAllergens },
-                  setSelectedAllergens: { setSelectedAllergens },
-                  tabBarLabel: "Settings",
-                  tabBarIcon: () => (
-                    <MaterialIcons name="settings" size={24} color="black" />
-                  ),
-                }}
-              />
-            </Tab.Navigator>
-          </NavigationContainer>
-        </>
+        <NavigationContainer>
+          <Tab.Navigator
+            shifting={true}
+            initialRouteName="Home"
+            activeColor="black"
+            inactiveColor="grey"
+            barStyle={{ backgroundColor: "white" }}
+          >
+            <Tab.Screen
+              name="Home"
+              component={HomeScreenNavigator}
+              options={{
+                tabBarLabel: "Hem",
+                tabBarIcon: () => (
+                  <AntDesign name="home" size={24} color="black" />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Scan"
+              component={ScannerScreenNavigator}
+              options={{
+                tabBarBadge: true,
+                tabBarLabel: "ScannerPage",
+                tabBarIcon: () => (
+                  <Ionicons name="barcode-outline" size={24} color="black" />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Report"
+              component={ReportScreenNavigator}
+              options={{
+                tabBarLabel: "Support",
+                tabBarIcon: () => (
+                  <MaterialIcons name="support-agent" size={24} color="black" />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Settings"
+              children={() => (
+                <Settings
+                  selectedAllergens={selectedAllergens}
+                  setSelectedAllergens={setSelectedAllergens}
+                  deleteAllergen={deleteAllergen}
+                />
+              )}
+              options={{
+                selectedAllergens: { selectedAllergens },
+                setSelectedAllergens: { setSelectedAllergens },
+                tabBarLabel: "Settings",
+                tabBarIcon: () => (
+                  <MaterialIcons name="settings" size={24} color="black" />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
       )}
-    </>
+    </ApplicationProvider>
   );
 };
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
 
 const Home = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text> THIS IS THE HOME PAGE </Text>
+    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text category="h1">HOME</Text>
       <TouchableOpacity
         onPress={() =>
           navigation.navigate("NestedHome", { msg: "From Screen 1" })
@@ -13,7 +14,7 @@ const Home = ({ navigation }) => {
       >
         <Text style={styles.buttonText}>Click Me!</Text>
       </TouchableOpacity>
-    </View>
+    </Layout>
   );
 };
 
