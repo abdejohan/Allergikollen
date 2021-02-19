4649815607ac15284603c3b355252ca649ad4bb9
Johan Abde <abdejohan@gmail.com>
2021-02-19 17:28:36 +0100

    Add api call to database for product information

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -4,6 +4,32 @@ import { API_KEY } from "@env";
 import axios from "axios";
 
 const ProductScreen = ({ route }) => {
+  const QRCODE = route.params.qrcode; // This data comes from the Scanner Page.
+  const [product, setProduct] = useState({});
+  const API_URL = `https://api.dabas.com/DABASService/V2/article/gtin/0${QRCODE}/JSON?apikey=${API_KEY}`;
+
+  const fetchProduct = async () => {
+    try {
+      await axios
+        .get(API_URL, {
+          headers: {
+            "Access-Control-Allow-Origin": "85.224.48.210",
+          },
+        })
+        .then((res) => {
+          const response = res.data.Produktkod;
+          setProduct({});
+        });
+    } catch (error) {
+      alert(`ERROR MESSAGE: ${error}`);
+      console.log(`ERROR MESSAGE: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
   return (
     <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text category="h2">{product.artikelbenamning}</Text>
