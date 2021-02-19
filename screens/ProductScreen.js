aa1ba4fec3e221e11ef4dd89f76a690896cb0cee
Johan Abde <abdejohan@gmail.com>
2021-02-19 20:46:25 +0100

    Add layout for when a product don´t exist in db

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Layout, Text } from "@ui-kitten/components";
+import { Layout, Text, Button } from "@ui-kitten/components";
 import { Image, StyleSheet } from "react-native";
 
 import { API_KEY } from "@env";
@@ -20,17 +20,22 @@ const ProductScreen = ({ route }) => {
         })
         .then((res) => {
           const response = res.data;
-          setProduct({
-            varumarke: response.Varumarke.Varumarke,
-            tillverkare: response.Varumarke.Tillverkare.Namn,
-            img: response.Bilder[0].Lank,
-            tillverkningslander: response.Tillverkningslander[0].Land,
-            allergener: response.Allergener,
-            Ingredienser: response.Ingredienser,
-            artikelbenamning: response.Artikelbenamning,
-            huvudgruppBenamning: response.Varugrupp.HuvudgruppBenamning,
-            ingrediensforteckning: response.Ingrediensforteckning,
-          });
+          if (Object.keys(response).length > 0) {
+            setProduct({
+              varumarke: response.Varumarke.Varumarke,
+              tillverkare: response.Varumarke.Tillverkare.Namn,
+              img: response.Bilder[0].Lank,
+              tillverkningslander: response.Tillverkningslander[0].Land,
+              allergener: response.Allergener,
+              Ingredienser: response.Ingredienser,
+              artikelbenamning: response.Artikelbenamning,
+              huvudgruppBenamning: response.Varugrupp.HuvudgruppBenamning,
+              ingrediensforteckning: response.Ingrediensforteckning,
+            });
+          } else {
+            setProduct({});
+          }
+          console.log("response" + response);
         });
       console.log(product);
     } catch (error) {
@@ -45,11 +50,26 @@ const ProductScreen = ({ route }) => {
 
   return (
     <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text category="c2">{product.varumarke}</Text>
-      <Text category="h4">{product.tillverkare}</Text>
-      <Text category="c2">{product.artikelbenamning}</Text>
-      <Text category="c1">{product.tillverkningslander}</Text>
-      <Image source={{ url: product.img }} style={styles.img1} />
+      {Object.keys(product).length > 0 ? (
+        <>
+          <Text category="c2">{product.varumarke}</Text>
+          <Text category="h4">{product.tillverkare}</Text>
+          <Text category="c2">{product.artikelbenamning}</Text>
+          <Text category="c1">{product.tillverkningslander}</Text>
+          <Image source={{ url: product.img }} style={styles.img1} />
+          <Button onPress={() => alert("button pressed")}>BUTTON</Button>
+        </>
+      ) : (
+        <>
+          <Text category="h1">Produkt saknas!</Text>
+          <Text category="h6">
+            Produkten du har scannat verkar inte finnas hos oss.
+          </Text>
+          <Button onPress={() => alert("button pressed")}>
+            Hjälp oss lägg till den!
+          </Button>
+        </>
+      )}
     </Layout>
   );
 };
