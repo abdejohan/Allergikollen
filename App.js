93d777b9376b3fe136a07813b8a6d912cfa0831b
Johan Abde <abdejohan@gmail.com>
2021-02-20 14:40:55 +0100

    Import Kitten Ui icons

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { createMaterialBottomTabNavigator } from "@react-navigation/material-bot
 import { Ionicons } from "@expo/vector-icons";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import { MaterialIcons } from "@expo/vector-icons";
-import { ApplicationProvider } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 // these handle screens for both bottom and top navigation
 import Settings from "./pages/Settings";
@@ -32,71 +33,78 @@ const App = () => {
   };
 
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      {!loaded ? (
-        <IntroPage />
-      ) : (
-        <NavigationContainer>
-          <Tab.Navigator
-            shifting={true}
-            initialRouteName="Home"
-            activeColor="black"
-            inactiveColor="grey"
-            barStyle={{ backgroundColor: "white" }}
-          >
-            <Tab.Screen
-              name="Home"
-              component={HomeScreenNavigator}
-              options={{
-                tabBarLabel: "Hem",
-                tabBarIcon: () => (
-                  <AntDesign name="home" size={24} color="black" />
-                ),
-              }}
-            />
-            <Tab.Screen
-              name="Scan"
-              component={ScannerScreenNavigator}
-              options={{
-                tabBarBadge: true,
-                tabBarLabel: "ScannerPage",
-                tabBarIcon: () => (
-                  <Ionicons name="barcode-outline" size={24} color="black" />
-                ),
-              }}
-            />
-            <Tab.Screen
-              name="Report"
-              component={ReportScreenNavigator}
-              options={{
-                tabBarLabel: "Support",
-                tabBarIcon: () => (
-                  <MaterialIcons name="support-agent" size={24} color="black" />
-                ),
-              }}
-            />
-            <Tab.Screen
-              name="Settings"
-              children={() => (
-                <Settings
-                  selectedAllergens={selectedAllergens}
-                  setSelectedAllergens={setSelectedAllergens}
-                  deleteAllergen={deleteAllergen}
-                />
-              )}
-              options={{
-                selectedAllergens: { selectedAllergens },
-                setSelectedAllergens: { setSelectedAllergens },
-                tabBarLabel: "Settings",
-                tabBarIcon: () => (
-                  <MaterialIcons name="settings" size={24} color="black" />
-                ),
-              }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
-      )}
-    </ApplicationProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        {!loaded ? (
+          <IntroPage />
+        ) : (
+          <NavigationContainer>
+            <Tab.Navigator
+              shifting={true}
+              initialRouteName="Home"
+              activeColor="black"
+              inactiveColor="grey"
+              barStyle={{ backgroundColor: "white" }}
+            >
+              <Tab.Screen
+                name="Home"
+                component={HomeScreenNavigator}
+                options={{
+                  tabBarLabel: "Hem",
+                  tabBarIcon: () => (
+                    <AntDesign name="home" size={24} color="black" />
+                  ),
+                }}
+              />
+              <Tab.Screen
+                name="Scan"
+                component={ScannerScreenNavigator}
+                options={{
+                  tabBarBadge: true,
+                  tabBarLabel: "ScannerPage",
+                  tabBarIcon: () => (
+                    <Ionicons name="barcode-outline" size={24} color="black" />
+                  ),
+                }}
+              />
+              <Tab.Screen
+                name="Report"
+                component={ReportScreenNavigator}
+                options={{
+                  tabBarLabel: "Support",
+                  tabBarIcon: () => (
+                    <MaterialIcons
+                      name="support-agent"
+                      size={24}
+                      color="black"
+                    />
+                  ),
+                }}
+              />
+              <Tab.Screen
+                name="Settings"
+                children={() => (
+                  <Settings
+                    selectedAllergens={selectedAllergens}
+                    setSelectedAllergens={setSelectedAllergens}
+                    deleteAllergen={deleteAllergen}
+                  />
+                )}
+                options={{
+                  selectedAllergens: { selectedAllergens },
+                  setSelectedAllergens: { setSelectedAllergens },
+                  tabBarLabel: "Settings",
+                  tabBarIcon: () => (
+                    <MaterialIcons name="settings" size={24} color="black" />
+                  ),
+                }}
+              />
+            </Tab.Navigator>
+          </NavigationContainer>
+        )}
+      </ApplicationProvider>
+    </>
   );
 };
 
