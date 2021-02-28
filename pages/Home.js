e64e02bfb7893dc9b19367018f538aac42e22e5d
Erik Axelsson <e.axelsson@tutanota.com>
2021-02-28 20:41:12 +0100

    add styling to homescreen

--- pages/Home.js
+++ pages/Home.js
@@ -1,42 +1,42 @@
 import React, { useState, useEffect } from "react";
-import { TouchableOpacity, StyleSheet } from "react-native";
+import { ImageBackground, TouchableOpacity, StyleSheet } from "react-native";
 import { Layout, Text } from "@ui-kitten/components";
 import { Sizing } from "../styles/index";
 
+// const image = { uri: "../assets/allergikollenbackground.jpeg" };
+
 const Home = ({ navigation }) => {
   return (
-    <Layout style={{ flex: 1 }}>
-      <Layout style={styles.homeContainer}>
-        <Layout style={styles.headerContainer}>
-          <Text category="h1">Allergikollen</Text>
-        </Layout>
-        <Layout style={styles.somethingContainer}>
-          <Text category="h1">Something!</Text>
-        </Layout>
-      </Layout>
-    </Layout>
+    <ImageBackground
+      source={require("../assets/allergikollenbackground.jpeg")}
+      style={styles.homeContainer}
+    >
+      <Text style={styles.headerContainer} category="h1">
+        Allergikollen
+      </Text>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   homeContainer: {
     flex: 1,
-    backgroundColor: "rgba(219,211,173, 0.3)",
+    width: "100%",
+    height: "100%",
     flexDirection: "column",
     justifyContent: "space-around",
-    alignItems: "stretch",
+    // alignItems: "stretch",
   },
   headerContainer: {
-    flex: 2,
-    backgroundColor: "rgba(219,211,173, 0.3)",
+    color: "white",
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
   },
-  somethingContainer: {
-    flex: 3,
-    backgroundColor: "rgba(219,211,173, 0.3)",
-  },
+  // somethingContainer: {
+  //   flex: 3,
+  //   backgroundColor: "rgba(219,211,173, 0.3)",
+  // },
   // text: {
   //   color: "#000",
   //   fontWeight: "700",
