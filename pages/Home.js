b1e06bdec2fee4b477d683075cfff2b5fd405310
Erik Axelsson <e.axelsson@tutanota.com>
2021-02-28 14:56:41 +0100

    add basic intro and start page

--- pages/Home.js
+++ pages/Home.js
@@ -5,27 +5,52 @@ import { Sizing } from "../styles/index";
 
 const Home = ({ navigation }) => {
   return (
-    <Layout style={Sizing.Screen}>
-      <Text category="h1">HOME</Text>
+    <Layout style={{ flex: 1 }}>
+      <Layout style={styles.homeContainer}>
+        <Layout style={styles.headerContainer}>
+          <Text category="h1">Allergikollen</Text>
+        </Layout>
+        <Layout style={styles.somethingContainer}>
+          <Text category="h1">Something!</Text>
+        </Layout>
+      </Layout>
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    color: "#000",
-    fontWeight: "700",
-    fontSize: 30,
+  homeContainer: {
+    flex: 1,
+    backgroundColor: "rgba(219,211,173, 0.3)",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "stretch",
   },
-  button: {
-    backgroundColor: "#0275d8",
-    paddingVertical: 5,
-    paddingHorizontal: 10,
+  headerContainer: {
+    flex: 2,
+    backgroundColor: "rgba(219,211,173, 0.3)",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
   },
-  buttonText: {
-    color: "#fff",
-    fontSize: 25,
+  somethingContainer: {
+    flex: 3,
+    backgroundColor: "rgba(219,211,173, 0.3)",
   },
+  // text: {
+  //   color: "#000",
+  //   fontWeight: "700",
+  //   fontSize: 30,
+  // },
+  // button: {
+  //   backgroundColor: "#0275d8",
+  //   paddingVertical: 5,
+  //   paddingHorizontal: 10,
+  // },
+  // buttonText: {
+  //   color: "#fff",
+  //   fontSize: 25,
+  // },
 });
 
 export default Home;
--- pages/IntroPage.js
+++ pages/IntroPage.js
@@ -1,13 +1,37 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { Sizing } from "../styles/index";
+import { Spinner } from "@ui-kitten/components";
 
 const IntroPage = () => {
   return (
     <View style={Sizing.Screen}>
-      <Text> WELCOME </Text>
+      <View>
+        <View>
+          <Text> Allergikollen </Text>
+        </View>
+        <View>
+          <Spinner />
+        </View>
+      </View>
     </View>
   );
 };
+
+// const styles = StyleSheet.create({
+//   introContainer: {
+//     flex: 1,
+//     backgroundColor: "rgba(219,211,173, 0.3)",
+//     flexDirection: "column",
+//     justifyContent: "space-around",
+//     alignItems: "stretch",
+//   },
+//   header: {
+//     flex: 1,
+//   },
+//   body: {
+//     flex: 1,
+//   },
+// });
 
 export default IntroPage;
