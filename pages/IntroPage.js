3ceb79a34ca85bac3ee56a611bb5002acf1d337f
Erik Axelsson <e.axelsson@tutanota.com>
2021-03-03 18:44:19 +0100

    add introPage styling

--- pages/IntroPage.js
+++ pages/IntroPage.js
@@ -1,27 +1,51 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Sizing } from "../styles/index";
 import { Layout, Spinner, Text } from "@ui-kitten/components";
 
 const IntroPage = () => {
   return (
-    <View style={Sizing.Screen}>
-      <Layout>
-        <Text>Allergikollen</Text>
-        <Spinner />
+    <View style={styles.introContainer}>
+      <Layout style={styles.stylingContainer}>
+        <Text style={styles.emptyContainer}></Text>
+
+        <Text style={styles.headlineContainer} category="h1">
+          Allergikollen
+        </Text>
+        <Layout style={styles.spinnerContainer}>
+          <Spinner status="basic" />
+        </Layout>
+        <Text style={styles.emptyContainer}></Text>
       </Layout>
     </View>
   );
 };
 
-// const styles = StyleSheet.create({
-//   introContainer: {
-//     flex: 1,
-//     backgroundColor: "blue",
-//     flexDirection: "column",
-//     justifyContent: "space-around",
-//     alignItems: "stretch",
-//   },
-// });
+const styles = StyleSheet.create({
+  introContainer: {
+    flex: 1,
+  },
+  stylingContainer: {
+    flex: 1,
+    color: "white",
+    backgroundColor: "black",
+    width: "100%",
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  headlineContainer: {
+    flex: 1,
+    color: "white",
+  },
+  spinnerContainer: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  emptyContainer: {
+    flex: 2,
+  },
+});
 
 export default IntroPage;
